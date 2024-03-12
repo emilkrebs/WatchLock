2da7079ed901476c36ad55b508dadf78d03be378
Emil Krebs <emil.krebs@outlook.de>
2024-03-12 22:29:57 +0100

    version update

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
