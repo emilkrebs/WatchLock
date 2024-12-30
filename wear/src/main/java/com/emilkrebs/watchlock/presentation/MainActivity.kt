dbf8dede562ecb1affae12a29820fe39686eadf2
Emil Krebs <emil.krebs@outlook.de>
2024-12-30 15:05:41 +0100

    added a horizontal pager and a page indicator

--- wear/src/main/java/com/emilkrebs/watchlock/presentation/MainActivity.kt
+++ wear/src/main/java/com/emilkrebs/watchlock/presentation/MainActivity.kt
@@ -6,12 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DeviceUnknown
@@ -26,6 +35,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,20 +67,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun Preview(context: Context = LocalContext.current) {
-    WatchLockTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            StatusView(context)
-            Spacer(modifier = Modifier.height(18.dp))
-            LockView(context)
-        }
-    }
+    Connected(context)
 }
 
 @Composable
@@ -96,19 +94,78 @@ fun WearApp(context: Context) {
 
 @Composable
 fun Connected(context: Context) {
+    val pagerState = rememberPagerState(pageCount = {
+        2
+    })
+
     WatchLockTheme {
-        Column(
+        Box (
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
                 .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            StatusView(context)
-            Spacer(modifier = Modifier.height(18.dp))
-            LockView(context)
+        ){
+            HorizontalPager(state = pagerState) { page ->
+                when (page) {
+                    0 -> {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
+                                .background(MaterialTheme.colors.background),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            StatusView(context)
+                            Spacer(modifier = Modifier.height(18.dp))
+                            LockView(context)
+                        }
+                    }
+
+                    1 -> {
+                        Screensharing(context)
+                    }
+                }
+            }
+
+            // Page Indicator
+            Row(
+                Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(pagerState.pageCount) { iteration ->
+                    val color =
+                        if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(8.dp)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun Screensharing(context: Context) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Screensharing",
+            color = MaterialTheme.colors.onBackground,
+            fontSize = 16.sp
+        )
     }
 }
 
